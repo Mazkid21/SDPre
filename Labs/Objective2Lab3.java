37b8ec205bb5a0240df5d21d0b8ff0aa4bd0510d
Mazkid21 <zmaraziti@yahoo.com>
2025-05-17 15:26:00 -0600

    complete objective2Lab1-3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+    public static void main(String[] args) {
+        int birthYear = 1991; 
+        String birthMonth = "June"; 
+        Boolean isTodayWeekend = true; 
+
+        System.out.println(birthYear);
+        System.out.println(birthMonth);
+        System.out.println(isTodayWeekend);
+    }
+}
